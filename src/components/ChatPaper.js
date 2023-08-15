c684d3f2c8b7267eae8bc8309b8c1c695d3c51b5
Johnx69 <hoanganh692004@gmail.com>
2023-08-15 23:32:50 +0700

    chat UI and solve the conflict pdfviewer

--- src/components/ChatPaper.js
+++ src/components/ChatPaper.js
@@ -1,56 +1,27 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import PdfViewer from './PdfViewer';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
+import React, { useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+
+import Chat from "./Chat";
+import PdfViewer from "./PdfViewer";
 
 const ChatPaper = () => {
-  const [chatMessages, setChatMessages] = useState([]);
-  const [message, setMessage] = useState('');
+    return (
+        <Container onScroll={false}>
+            <Row>
+                {/* Left Section - PDF Viewer */}
+                <Col md={6}>
+                    <h1>Name of the PDF</h1>
+                    <PdfViewer />
+                </Col>
 
-  const handleSendMessage = () => {
-    // Implement the logic to send the chat message to the backend or a state management system.
-    // For this example, we'll just update the local state.
-    if (message.trim() !== '') {
-      setChatMessages([...chatMessages, message]);
-      setMessage('');
-    }
-  };
-
-  return (
-    <Container>
-      <Row>
-        {/* Left Section - PDF Viewer */}
-        <Col md={6}>
-          <PdfViewer />
-        </Col>
-
-        {/* Right Section - Chat Block */}
-        <Col md={6}>
-          <div className="chat-block">
-            <div className="chat-messages">
-              {chatMessages.map((msg, index) => (
-                <div key={index} className="chat-message">
-                  {msg}
-                </div>
-              ))}
-            </div>
-            <div className="chat-input">
-              <input
-                type="text"
-                placeholder="Type your message..."
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              />
-              <Button variant="primary" onClick={handleSendMessage}>
-                Send
-              </Button>
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
+                {/* Right Section - Chat Block */}
+                <Col md={6}>
+                    <h1>Chat Paper</h1>
+                    <Chat />
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default ChatPaper;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,29 @@
-import React from 'react';
-import { Navbar, Container, Nav, Image} from 'react-bootstrap';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 const NavbarComponent = () => {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        {/* Brand Image */}
-        <Navbar.Brand href="/">
-          <img
-            src="https://cinnamon.is/en/wp-content/uploads/sites/2/2020/04/CinnamonAI_1280x670.jpg" 
-            alt="Brand Logo"
-            height="60"
-          />
-        </Navbar.Brand>
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                {/* Brand Image */}
+                <Navbar.Brand href="/"></Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
 
-        {/* Profile Section */}
-        <Nav className="ml-auto">
-          <Nav.Link href="/profile">Profile</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
+                {/* Profile Section */}
+                <Nav className="ml-auto">
+                    <Nav.Link href="/profile">Profile</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default NavbarComponent;
